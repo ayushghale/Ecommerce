f7f0483abef253a9838819c439efbed16823c318
ayushGhale <ghaleayush03@gmail.com>
2024-04-17 15:35:01 +0545

    database

--- public/ecommerce.sql
+++ public/ecommerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 17, 2024 at 08:45 AM
+-- Generation Time: Apr 17, 2024 at 11:49 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
