07404d591dd0decaa2a3f3655328fa18d21340a4
ayushGhale <ghaleayush03@gmail.com>
2024-05-09 22:22:43 +0545

    stock

--- public/ecommerce.sql
+++ public/ecommerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 17, 2024 at 11:49 AM
+-- Generation Time: May 09, 2024 at 06:35 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -93,6 +93,28 @@ CREATE TABLE `favorites` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `inventories`
+--
+
+CREATE TABLE `inventories` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `product_id` bigint(20) UNSIGNED NOT NULL,
+  `stock` int(11) NOT NULL DEFAULT 1 COMMENT 'Product stock quantity.',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `inventories`
+--
+
+INSERT INTO `inventories` (`id`, `product_id`, `stock`, `created_at`, `updated_at`) VALUES
+(1, 2, 10, '2024-04-27 20:26:10', '2024-04-29 03:18:31'),
+(2, 3, 229, '2024-04-27 21:08:00', '2024-04-29 03:18:31');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `migrations`
 --
 
@@ -118,7 +140,9 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (9, '2024_03_22_134513_create_carts_table', 1),
 (10, '2024_03_22_134520_create_payments_table', 1),
 (11, '2024_03_22_134525_create_reviews_table', 1),
-(12, '2024_03_31_022457_create_favorites_table', 1);
+(12, '2024_03_31_022457_create_favorites_table', 1),
+(13, '2024_04_18_171049_create_otp_verifications_table', 2),
+(15, '2024_04_28_012624_create_inventories_table', 3);
 
 -- --------------------------------------------------------
 
@@ -141,14 +165,8 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `user_id`, `total`, `uCode`, `status`, `created_at`, `updated_at`) VALUES
-(1, 2, 1879.94, 'zkZcley5ZY8bMuhmiQRGhmT73ZtOiX80', 'completed', '2024-03-25 23:12:00', '2024-04-16 04:01:17'),
-(2, 2, 1879.94, 'ANX4lk6RMGH5DmLaZVf3CaLEpu2n8hCb', 'pending', '2024-04-15 22:55:59', '2024-04-15 22:55:59'),
-(3, 2, 1879.94, 'e15WKdimVXKuV7rzW9En5velmZsoG3HU', 'completed', '2024-04-15 22:56:31', '2024-04-16 03:57:42'),
-(4, 2, 1879.94, 'iwGClwyEn2dFZjT8RvupSZnYpR31gUjs', 'completed', '2024-04-15 23:01:44', '2024-04-16 03:49:27'),
-(5, 2, 1879.94, 'zCk7Ls18mylSGrkM3zqTc1vzhBJ8ixv4', 'pending', '2024-04-16 05:33:45', '2024-04-16 05:33:45'),
-(6, 2, 1879.94, 'qhmS0JZgi6iXxRk1XZVSi8xqz8u1TqD3', 'pending', '2024-04-16 05:33:47', '2024-04-16 05:33:47'),
-(7, 2, 1879.94, 'upzisyZjFldsLhjOJBlB5cQtOJoNcUua', 'pending', '2024-04-16 05:33:48', '2024-04-16 05:33:48'),
-(8, 2, 1879.94, 'lvhYc6JqQBOrXN34KCPsxBuMo5Q6Sd3v', 'pending', '2024-04-16 05:33:53', '2024-04-16 05:33:53');
+(1, 1, 2246, 'cYHHTO290TMjW3fpzPdgybfiYpUjbVx6', 'pending', '2024-04-27 21:27:41', '2024-04-27 21:27:41'),
+(2, 1, 2246, 'FZRwnkgmX4GOIblaTnlQ73wtAVHJOBfb', 'pending', '2024-04-29 03:18:31', '2024-04-29 03:18:31');
 
 -- --------------------------------------------------------
 
@@ -173,30 +191,33 @@ CREATE TABLE `order_details` (
 --
 
 INSERT INTO `order_details` (`id`, `order_id`, `product_id`, `quantity`, `total`, `uCode`, `status`, `created_at`, `updated_at`) VALUES
-(1, 1, 1, 3, 1797, 'zkZcley5ZY8bMuhmiQRGhmT73ZtOiX80', 'pending', '2024-03-25 23:12:00', '2024-03-25 23:12:00'),
-(2, 1, 3, 1, 19, 'zkZcley5ZY8bMuhmiQRGhmT73ZtOiX80', 'pending', '2024-03-25 23:12:00', '2024-03-25 23:12:00'),
-(3, 1, 5, 2, 58, 'zkZcley5ZY8bMuhmiQRGhmT73ZtOiX80', 'pending', '2024-03-25 23:12:00', '2024-03-25 23:12:00'),
-(4, 2, 1, 3, 1797, 'ANX4lk6RMGH5DmLaZVf3CaLEpu2n8hCb', 'pending', '2024-04-15 22:55:59', '2024-04-15 22:55:59'),
-(5, 2, 3, 1, 19, 'ANX4lk6RMGH5DmLaZVf3CaLEpu2n8hCb', 'pending', '2024-04-15 22:55:59', '2024-04-15 22:55:59'),
-(6, 2, 5, 2, 58, 'ANX4lk6RMGH5DmLaZVf3CaLEpu2n8hCb', 'pending', '2024-04-15 22:55:59', '2024-04-15 22:55:59'),
-(7, 3, 1, 3, 1797, 'e15WKdimVXKuV7rzW9En5velmZsoG3HU', 'pending', '2024-04-15 22:56:31', '2024-04-15 22:56:31'),
-(8, 3, 3, 1, 19, 'e15WKdimVXKuV7rzW9En5velmZsoG3HU', 'pending', '2024-04-15 22:56:31', '2024-04-15 22:56:31'),
-(9, 3, 5, 2, 58, 'e15WKdimVXKuV7rzW9En5velmZsoG3HU', 'pending', '2024-04-15 22:56:31', '2024-04-15 22:56:31'),
-(10, 4, 1, 3, 1797, 'iwGClwyEn2dFZjT8RvupSZnYpR31gUjs', 'pending', '2024-04-15 23:01:44', '2024-04-15 23:01:44'),
-(11, 4, 3, 1, 19, 'iwGClwyEn2dFZjT8RvupSZnYpR31gUjs', 'pending', '2024-04-15 23:01:44', '2024-04-15 23:01:44'),
-(12, 4, 5, 2, 58, 'iwGClwyEn2dFZjT8RvupSZnYpR31gUjs', 'pending', '2024-04-15 23:01:44', '2024-04-15 23:01:44'),
-(13, 5, 1, 3, 1797, 'zCk7Ls18mylSGrkM3zqTc1vzhBJ8ixv4', 'pending', '2024-04-16 05:33:45', '2024-04-16 05:33:45'),
-(14, 5, 3, 1, 19, 'zCk7Ls18mylSGrkM3zqTc1vzhBJ8ixv4', 'pending', '2024-04-16 05:33:45', '2024-04-16 05:33:45'),
-(15, 5, 5, 2, 58, 'zCk7Ls18mylSGrkM3zqTc1vzhBJ8ixv4', 'pending', '2024-04-16 05:33:45', '2024-04-16 05:33:45'),
-(16, 6, 1, 3, 1797, 'qhmS0JZgi6iXxRk1XZVSi8xqz8u1TqD3', 'pending', '2024-04-16 05:33:47', '2024-04-16 05:33:47'),
-(17, 6, 3, 1, 19, 'qhmS0JZgi6iXxRk1XZVSi8xqz8u1TqD3', 'pending', '2024-04-16 05:33:47', '2024-04-16 05:33:47'),
-(18, 6, 5, 2, 58, 'qhmS0JZgi6iXxRk1XZVSi8xqz8u1TqD3', 'pending', '2024-04-16 05:33:47', '2024-04-16 05:33:47'),
-(19, 7, 1, 3, 1797, 'upzisyZjFldsLhjOJBlB5cQtOJoNcUua', 'pending', '2024-04-16 05:33:48', '2024-04-16 05:33:48'),
-(20, 7, 3, 1, 19, 'upzisyZjFldsLhjOJBlB5cQtOJoNcUua', 'pending', '2024-04-16 05:33:48', '2024-04-16 05:33:48'),
-(21, 7, 5, 2, 58, 'upzisyZjFldsLhjOJBlB5cQtOJoNcUua', 'pending', '2024-04-16 05:33:48', '2024-04-16 05:33:48'),
-(22, 8, 1, 3, 1797, 'lvhYc6JqQBOrXN34KCPsxBuMo5Q6Sd3v', 'pending', '2024-04-16 05:33:53', '2024-04-16 05:33:53'),
-(23, 8, 3, 1, 19, 'lvhYc6JqQBOrXN34KCPsxBuMo5Q6Sd3v', 'pending', '2024-04-16 05:33:53', '2024-04-16 05:33:53'),
-(24, 8, 5, 2, 58, 'lvhYc6JqQBOrXN34KCPsxBuMo5Q6Sd3v', 'pending', '2024-04-16 05:33:53', '2024-04-16 05:33:53');
+(1, 1, 2, 2, 2000, 'cYHHTO290TMjW3fpzPdgybfiYpUjbVx6', 'pending', '2024-04-27 21:27:41', '2024-04-27 21:27:41'),
+(2, 1, 3, 2, 246, 'cYHHTO290TMjW3fpzPdgybfiYpUjbVx6', 'pending', '2024-04-27 21:27:41', '2024-04-27 21:27:41'),
+(3, 2, 2, 2, 2000, 'FZRwnkgmX4GOIblaTnlQ73wtAVHJOBfb', 'pending', '2024-04-29 03:18:31', '2024-04-29 03:18:31'),
+(4, 2, 3, 2, 246, 'FZRwnkgmX4GOIblaTnlQ73wtAVHJOBfb', 'pending', '2024-04-29 03:18:31', '2024-04-29 03:18:31');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `otp_verifications`
+--
+
+CREATE TABLE `otp_verifications` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `otp` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `otp_verifications`
+--
+
+INSERT INTO `otp_verifications` (`id`, `email`, `type`, `otp`, `created_at`, `updated_at`) VALUES
+(1, 'ghaleayush03@gmail.com', 'emailVerification', '2974', '2024-04-22 00:36:09', '2024-04-22 00:36:09'),
+(2, 'admin@gmail.com', 'emailVerification', '1488', '2024-04-22 16:59:00', '2024-04-22 16:59:00');
 
 -- --------------------------------------------------------
 
@@ -233,14 +254,8 @@ CREATE TABLE `payments` (
 --
 
 INSERT INTO `payments` (`id`, `order_id`, `user_id`, `payment_method`, `payment_amount`, `transactionCode`, `payment_status`, `created_at`, `updated_at`) VALUES
-(1, 1, 2, 'cash in hand', 1879.94, NULL, 'completed', '2024-03-25 23:12:00', '2024-04-16 04:00:57'),
-(2, 2, 2, 'cash in hand', 1879.94, NULL, 'pending', '2024-04-15 22:55:59', '2024-04-15 22:55:59'),
-(3, 3, 2, 'cash in hand', 1879.94, NULL, 'completed', '2024-04-15 22:56:31', '2024-04-16 03:57:21'),
-(4, 4, 2, 'esewa', 1879.94, NULL, 'completed', '2024-04-15 23:01:44', '2024-04-16 03:49:27'),
-(5, 5, 2, 'cash in hand', 1879.94, NULL, 'pending', '2024-04-16 05:33:45', '2024-04-16 05:33:45'),
-(6, 6, 2, 'cash in hand', 1879.94, NULL, 'pending', '2024-04-16 05:33:47', '2024-04-16 05:33:47'),
-(7, 7, 2, 'cash in hand', 1879.94, NULL, 'pending', '2024-04-16 05:33:48', '2024-04-16 05:33:48'),
-(8, 8, 2, 'cash in hand', 1879.94, NULL, 'pending', '2024-04-16 05:33:53', '2024-04-16 05:33:53');
+(1, 1, 1, 'cash in hand', 2246, NULL, 'pending', '2024-04-27 21:27:41', '2024-04-27 21:27:41'),
+(2, 2, 1, 'cash in hand', 2246, NULL, 'pending', '2024-04-29 03:18:31', '2024-04-29 03:18:31');
 
 -- --------------------------------------------------------
 
@@ -283,18 +298,8 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `image`, `description`, `price`, `category_id`, `created_at`, `updated_at`) VALUES
-(1, 'Smartphone', '/site/uploads/product/1794576851319942.jpg', 'A powerful smartphone with the latest features.', '599.99', 1, '2024-03-25 21:00:51', '2024-03-25 21:00:51'),
-(2, 'Laptop', '/site/uploads/product/1794576926455491.jpg', 'High-performance laptop for work and entertainment.', '999.99', 1, '2024-03-25 21:02:03', '2024-03-25 21:02:03'),
-(3, 'T-shirt', '/site/uploads/product/1794576951555076.jpg', 'Comfortable cotton t-shirt for everyday wear.', '19.99', 2, '2024-03-25 21:02:27', '2024-03-25 21:02:27'),
-(4, 'Jeans', '/site/uploads/product/1794576968785973.jpg', 'Classic denim jeans for a timeless look.', '39.99', 2, '2024-03-25 21:02:44', '2024-03-25 21:02:44'),
-(5, 'Python Programming for Beginners', '/site/uploads/product/1794576986090706.jpg', 'A beginner-friendly guide to Python programming language.', '29.99', 3, '2024-03-25 21:03:00', '2024-03-25 21:03:00'),
-(6, 'Cooking Utensil Set', '/site/uploads/product/1794577005007313.jpg', 'Complete set of cooking utensils for your kitchen.', '49.99', 4, '2024-03-25 21:03:18', '2024-03-25 21:03:18'),
-(7, 'Bluetooth Speaker', '/site/uploads/product/1794577028407871.jpg', 'Portable bluetooth speaker for music on the go.', '79.99', 1, '2024-03-25 21:03:40', '2024-03-25 21:03:40'),
-(8, 'Hoodie', '/site/uploads/product/1794577045527597.jpg', 'Warm and stylish hoodie for chilly days.', '29.99', 2, '2024-03-25 21:03:57', '2024-03-25 21:03:57'),
-(9, 'Novel: The Great Gatsby', '/site/uploads/product/1794577067069159.jpg', 'Classic novel by F. Scott Fitzgerald.', '12.99', 3, '2024-03-25 21:04:17', '2024-03-25 21:04:17'),
-(10, 'Coffee Maker', '/site/uploads/product/1794577103730722.jpg', 'Automatic coffee maker for brewing your favorite coffee.', '69.99', 4, '2024-03-25 21:04:52', '2024-03-25 21:04:52'),
-(12, 'new', '/site/uploads/product/1796482320676717.png', 'asd', '199999', 2, '2024-04-15 21:40:13', '2024-04-15 21:47:29'),
-(13, 'test', '/site/uploads/product/1796546765391788.png', 'sfrgesrg', '32453', 2, '2024-04-16 14:51:48', '2024-04-16 14:51:48');
+(2, 'phone', '/site/uploads/product/1797542663145434.jpg', 'asdasdsa', '1000', 1, '2024-04-27 20:26:10', '2024-04-27 20:26:10'),
+(3, 'short', '/site/uploads/product/1797545295437600.jpg', 'asdsda', '123', 2, '2024-04-27 21:08:00', '2024-04-27 21:08:00');
 
 -- --------------------------------------------------------
 
@@ -323,7 +328,7 @@ CREATE TABLE `reviews` (
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(255) NOT NULL,
-  `image` varchar(255) NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
   `location` varchar(255) NOT NULL,
   `contact_number` varchar(255) NOT NULL,
@@ -340,14 +345,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `image`, `email`, `location`, `contact_number`, `email_verified_at`, `password`, `role`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'admin', '', 'admin@gmail.com', 'pkr', '9999999999', NULL, '$2y$12$FUzqQyBgt8r6MoyBm49wDeHeY6jvsZhzn/XdO5Q4doAW4JS.JrKJG', 1, NULL, '2024-03-25 14:07:46', '2024-03-25 14:07:46'),
-(2, 'John Doe', 'john.jpg', 'john@example.com', 'New York', '1234567890', NULL, '$2y$12$FUzqQyBgt8r6MoyBm49wDeHeY6jvsZhzn/XdO5Q4doAW4JS.JrKJG', 0, NULL, NULL, NULL),
-(3, 'Jane Smith', 'jane.jpg', 'jane@example.com', 'Los Angeles', '9876543210', NULL, '$2y$12$FUzqQyBgt8r6MoyBm49wDeHeY6jvsZhzn/XdO5Q4doAW4JS.JrKJG', 0, NULL, NULL, NULL),
-(4, 'Michael Johnson', 'michael.jpg', 'michael@example.com', 'Chicago', '4567891230', NULL, '$2y$12$FUzqQyBgt8r6MoyBm49wDeHeY6jvsZhzn/XdO5Q4doAW4JS.JrKJG', 0, NULL, NULL, NULL),
-(5, 'Emily Davis', 'emily.jpg', 'emily@example.com', 'San Francisco', '7891234560', NULL, '$2y$12$FUzqQyBgt8r6MoyBm49wDeHeY6jvsZhzn/XdO5Q4doAW4JS.JrKJG', 0, NULL, NULL, NULL),
-(6, 'Chris Wilson', 'chris.jpg', 'chris@example.com', 'Seattle', '6543217890', NULL, '$2y$12$FUzqQyBgt8r6MoyBm49wDeHeY6jvsZhzn/XdO5Q4doAW4JS.JrKJG', 0, NULL, NULL, NULL),
-(7, 'Sarah Brown', 'sarah.jpg', 'sarah@example.com', 'Miami', '3216549870', NULL, '$2y$12$FUzqQyBgt8r6MoyBm49wDeHeY6jvsZhzn/XdO5Q4doAW4JS.JrKJG', 0, NULL, NULL, NULL),
-(8, 'Ayush', '', 'user1@gmail.com', 'Nepal, Pokhara', '9819160357', NULL, '$2y$12$AAXVCJzMZKCoM8yTiumGjOWsw6T4IZeGQ1V0rAtZWRoOJXT64Hlbm', 0, NULL, '2024-04-15 20:16:38', '2024-04-15 20:25:04');
+(1, 'ayush', NULL, 'ghaleayush03@gmail.com', 'pkr', '9999000000', NULL, '$2y$12$7K1nDZCLl0N0BDBLzhoEJe.8PpJxQWUzzX4Cgoo0ugPLp6vt3tVp.', 0, NULL, '2024-04-22 00:36:09', '2024-04-22 00:36:09'),
+(2, 'admin', NULL, 'admin@gmail.com', 'pkr', '9999000009', NULL, '$2y$12$ihitSFcZlEhXxGSUjZaJ8.nKK5agS1sbNkKFiTXYtzmwmYEJhrWFS', 1, NULL, '2024-04-22 16:59:00', '2024-04-22 16:59:00');
 
 --
 -- Indexes for dumped tables
@@ -383,6 +382,13 @@ ALTER TABLE `favorites`
   ADD KEY `favorites_product_id_foreign` (`product_id`);
 
 --
+-- Indexes for table `inventories`
+--
+ALTER TABLE `inventories`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `inventories_product_id_foreign` (`product_id`);
+
+--
 -- Indexes for table `migrations`
 --
 ALTER TABLE `migrations`
@@ -402,6 +408,12 @@ ALTER TABLE `order_details`
   ADD PRIMARY KEY (`id`),
   ADD KEY `order_details_order_id_foreign` (`order_id`),
   ADD KEY `order_details_product_id_foreign` (`product_id`);
+
+--
+-- Indexes for table `otp_verifications`
+--
+ALTER TABLE `otp_verifications`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `password_reset_tokens`
@@ -456,7 +468,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `carts`
 --
 ALTER TABLE `carts`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `categories`
@@ -477,28 +489,40 @@ ALTER TABLE `favorites`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `inventories`
+--
+ALTER TABLE `inventories`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `order_details`
 --
 ALTER TABLE `order_details`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `otp_verifications`
+--
+ALTER TABLE `otp_verifications`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `payments`
 --
 ALTER TABLE `payments`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -510,7 +534,7 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `reviews`
@@ -522,7 +546,7 @@ ALTER TABLE `reviews`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -541,6 +565,12 @@ ALTER TABLE `carts`
 ALTER TABLE `favorites`
   ADD CONSTRAINT `favorites_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
   ADD CONSTRAINT `favorites_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `inventories`
+--
+ALTER TABLE `inventories`
+  ADD CONSTRAINT `inventories_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
 
 --
 -- Constraints for table `orders`
